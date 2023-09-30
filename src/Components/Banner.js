0fefae3837321a794d0554990c5ba41ffdb64053
habbkad <habibkadiri52@gmail.com>
2023-09-30 12:54:37 +0000

    done

---
+++ src/Components/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1692198669686-0a3959951e11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+        height={"400px"}
+        width={"100%"}
+      />
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>Footer</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+        aspernatur ex ad minus animi omnis vero nobis cum ipsum expedita facere
+        magnam quisquam recusandae laboriosam dolorem repudiandae nisi, eum
+        officiis!
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
