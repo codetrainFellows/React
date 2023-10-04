161605adc0e286d7e48da74a63229a0ca8b4cf99
habbkad <habibkadiri52@gmail.com>
2023-10-04 11:48:53 +0000

    upadated

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import Banner from "./Components/Banner";
 import Footer from "./Components/Footer";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Banner />
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
