f273269789aade57fa02b86f799e20d505cab31d
habbkad <habibkadiri52@gmail.com>
2023-10-04 13:03:13 +0000

    done with Create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import Banner from "./Components/Banner";
-import Footer from "./Components/Footer";
+import React, { Component } from "react";
+import Functional from "./Components/Functional";
+import Container from "react-bootstrap/Container";
+import { Row, Col } from "react-bootstrap";
 
-function App() {
-  return <div></div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [],
+    };
+  }
+
+  createStudent = (student) => {
+    this.setState({
+      students: [...this.state.students, student],
+    });
+  };
+
+  render() {
+    console.log(this.state.students);
+    return (
+      <Container fluid>
+        <Row>
+          <Col xs={12} md={6}>
+            <Functional createStudent={this.createStudent} />
+          </Col>
+          <Col xs={12} md={6}>
+            happy
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Functional.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const Functional = (props) => {
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState();
+  const [gen, setGen] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const student = {
+      email,
+      phone,
+      gen,
+    };
+
+    props.createStudent(student);
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Phone No.</Form.Label>
+          <Form.Control
+            type="tel"
+            placeholder="phone"
+            value={phone}
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Gen</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Gen"
+            value={gen}
+            onChange={(e) => {
+              setGen(e.target.value);
+            }}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Functional;
